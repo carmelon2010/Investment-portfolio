416724baf340ff66ed906e5c6e3661bef322ebd9
Uri Ben Eliezer <work@urib.me>
2026-02-24 20:45:07 +0200

    Add `id` field to `User` class and update constructor

--- src/com/example/portfolio/User.java
+++ src/com/example/portfolio/User.java
@@ -2,13 +2,16 @@ package com.example.portfolio;
 
 public class User {
     private final String username;
+    private final int id;
 
-    public User(String username) {
+    public User(String username, int id) {
         this.username = username;
+        this.id = id;
     }
 
     public String getUsername() {
         return username;
     }
+    public int getId() { return id;}
 }
 
