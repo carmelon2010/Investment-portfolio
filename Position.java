bb0ba4f8219fa9d37ede6550c3be03256f3ac2a9
Itay <itay3.8.2010@gmail.com>
2026-02-24 19:46:25 +0200

    added funcs

--- Position.java
+++ Position.java
@@ -3,4 +3,31 @@ public class Position {
     private int quantity;
     private Stock stock;
 
+    public Position(float originalPrice, int quantity, Stock stock) {
+        this.originalPrice = originalPrice;
+        this.quantity = quantity;
+        this.stock = stock;
+    }
+
+    public float getOriginalPrice() {
+        return originalPrice;
+    }
+    public float getCurrentPrice() {
+        return stock.getCurrentPrice();
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public Stock getStock() {
+        return stock;
+    }
+    public float getTotalValue() {
+        return quantity * stock.getCurrentPrice();
+    }
+    public float getProfit() {
+        return quantity * (stock.getCurrentPrice() - originalPrice);
+    }
+    public float getProfitPercentage() {
+        return (stock.getCurrentPrice() - originalPrice) / originalPrice * 100;
+    }
 }
